5f5f96cddb0fcfd41b76880a4ad67722bc720f3b
EC2 Default User <ec2-user@ip-172-31-34-133.us-east-2.compute.internal>
2018-03-15 13:49:33 +0000

    f

---
+++ db/migrate/20180315134753_remove_following_user.rb
@@ -0,0 +1,5 @@
+class RemoveFollowingUser < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :following
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310024359) do
+ActiveRecord::Schema.define(version: 20180315134753) do
 
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 20180310024359) do
     t.string "image"
     t.string "first_name"
     t.string "last_name"
-    t.string "following", default: "--- []\n"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
