cd31d0c1959d9af7071792d3e7f6cb73ea4dcb90
EC2 Default User <ec2-user@ip-172-31-34-133.us-east-2.compute.internal>
2018-03-29 08:02:23 +0000

    multiple submits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,30 @@
 
 $(document).ready( function() {// When the Dom is ready
     $(".alert").fadeOut(2000); //Add a fade out effect that will last for 2000 millisecond
-    
+    $('form').preventDoubleSubmission();
+
+
+
+
+
+$.fn.preventDoubleSubmission = function() {
+  $(this).on('submit',function(e){
+    var $form = $(this);
+
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+
+  // Keep chainability
+  return this;
+};
+
+
 
 });
 
@@ -32,3 +55,4 @@ $(document).ready( function() {// When the Dom is ready
 
 
 
+// jQuery plugin to prevent double submission of forms
---
+++ app/assets/javascripts/plugin.js
@@ -0,0 +1,23 @@
+//= require jquery
+//= require jquery_ujs
+// require turbolinks
+//= require_tree 
+
+
+// jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function() {
+  $(this).on('submit',function(e){
+    var $form = $(this);
+
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+
+  // Keep chainability
+  return this;
+};
