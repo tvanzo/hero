dafe9affb18db7f02b6460fca652212442ce61a7
EC2 Default User <ec2-user@ip-172-31-34-133.us-east-2.compute.internal>
2018-04-30 12:01:05 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,28 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+  
+  
+  
+   before_action :detect_device_format
+
+  private
+
+    def detect_device_format
+      case request.user_agent
+      when /iPad/i
+        request.variant = :tablet
+      when /iPhone/i
+        request.variant = :phone
+      when /Android/i && /mobile/i
+        request.variant = :phone
+      when /Android/i
+        request.variant = :tablet
+      when /Windows Phone/i
+        request.variant = :phone
+      end
+    end
 end
+  
+  
+
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,17 @@
 class HomeController < ApplicationController
     def index
+    respond_to do |format|
+      format.json
+      format.html          # /app/views/home/index.html.erb
+      format.html.phone    # /app/views/home/index.html+phone.erb
+      format.html.tablet   # /app/views/home/index.html+tablet.erb
+    end
+        
         if user_signed_in?
             redirect_to("/posts")
         else
             redirect_to("/users/sign_in")
         end
     end
-    def t
-    end
+    
 end
