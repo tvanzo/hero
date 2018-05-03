a91250366bc5f2c612701da7b05847b159e40c94
EC2 Default User <ec2-user@ip-172-31-34-133.us-east-2.compute.internal>
2018-05-03 09:01:21 +0000

    a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 
 //= require jquery
 //= require jquery_ujs
-
+//= require bootstrap-sprockets
 //= require_tree .
 
 
@@ -20,39 +20,4 @@
 /* global $ */
 
 
-$(document).ready( function() {// When the Dom is ready
-    $(".alert").fadeOut(2000); //Add a fade out effect that will last for 2000 millisecond
-    $('form').preventDoubleSubmission();
 
-
-
-
-
-$.fn.preventDoubleSubmission = function() {
-  $(this).on('submit',function(e){
-    var $form = $(this);
-
-    if ($form.data('submitted') === true) {
-      // Previously submitted - don't submit again
-      e.preventDefault();
-    } else {
-      // Mark it so that the next submit can be ignored
-      $form.data('submitted', true);
-    }
-  });
-
-  // Keep chainability
-  return this;
-};
-
-
-
-});
-
-
-
-
-
-
-
-// jQuery plugin to prevent double submission of forms
